0c3e2413929454b07e358379c52f1b7a43e2b79e
lynnjean <kimlynnjean@naver.com>
2022-01-03 16:08:27 +0900

    comment count

--- routes/post.js
+++ routes/post.js
@@ -213,15 +213,11 @@ var comment=function(req, res, next) {
 };
 
 var uncomment= async function(req, res, next) {
-  // const post = await Post.findById(req.post._id)
-
+  const post = await Post.findById(req.post._id)
   if(req.comment.author.toString() === req.payload.id.toString()){
       const comment=await Comment.find({_id: req.comment._id}).remove().exec()
-      // console.log(post.comments)
-      // post.comments.remove({comments:req.comment._id})
-      // post.updateOne({_id:req.post._id},{comments:req.post.comments},)
-      // console.log(post)
-      // comment.remove().exec()
+      post.comments.remove(req.comment._id)
+      await Post.findByIdAndUpdate(req.post._id,{comments:post.comments})
       return res.status(200).json({'message':'댓글이 삭제되었습니다.','status':'200'});
   } else {
       res.status(403).json({'message':'댓글 작성자만 댓글을 삭제할 수 있습니다.','status':'403'});
