910863fef3c9b912a8f9d94f209405b2a4822e35
lynnjean <kimlynnjean@naver.com>
2022-01-12 10:49:33 +0900

    roomlist fix

--- routes/chat.js
+++ routes/chat.js
@@ -66,7 +66,9 @@ router.post('/:accountname/chatroom',async function(req,res){
 })
 
 router.get('/roomList', async function(req,res) {
-    const rooms = await ChatRoom.find({myId:req.payload.id})
+    const me=await User.findById(req.payload.id)
+    const rooms = await ChatRoom.find({me:me.accountname})
+
     return res.status(200).json({rooms:rooms.map(rooms=>rooms.toChatRoomJSONFor())})
 })
 
